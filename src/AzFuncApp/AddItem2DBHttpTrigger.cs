9d463e18feb975b6df48016125d3eee6350022e9
Oscar Santos <oscarsantosmu@gmail.com>
2022-04-30 21:32:33 +0200

    update: save all coordinates in db with a get request

--- src/AzFuncApp/AddItem2DBHttpTrigger.cs
+++ src/AzFuncApp/AddItem2DBHttpTrigger.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Company.Function
 {
@@ -18,16 +20,50 @@ namespace Company.Function
             [CosmosDB(
                 databaseName: "my-database",
                 collectionName: "my-container",
-                ConnectionStringSetting = "CosmosDbConnectionString")]IAsyncCollector<dynamic> documentsOut, 
+                ConnectionStringSetting = "CosmosDbConnectionString")]IAsyncCollector<dynamic> documentsOut,
                 ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+
+            Dictionary<string, float> coordinatesDict = new();
+            List<string> coordinates = new();
+            for (int i = 1; i <= 4; i++)
+            {
+                string x = req.Query[$"x{i}"];
+                string y = req.Query[$"y{i}"];
+                coordinates.Add(x);
+                coordinates.Add(y);
+                coordinatesDict.Add($"x{i}", float.Parse(x, CultureInfo.InvariantCulture.NumberFormat));
+                coordinatesDict.Add($"y{i}", float.Parse(y, CultureInfo.InvariantCulture.NumberFormat));
+
+            }
 
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            string msg = null; // when everything works fine this value remains in null
+            foreach (string coordinate in coordinates)
+            {
+                if (string.IsNullOrEmpty(coordinate))
+                {
+                    msg = "please specify all coordinates so that they can be stored in the database.";
+                    break;
+                }
+            }
+
+            if (string.IsNullOrEmpty(msg))
+            {
+                // Add a JSON document to the output container.
+                await documentsOut.AddAsync(new
+                {
+                    // create a random ID
+                    id = System.Guid.NewGuid().ToString(),
+                    coordinates = coordinatesDict
+                });
+            }
 
             if (!string.IsNullOrEmpty(name))
             {
@@ -40,9 +76,9 @@ namespace Company.Function
                 });
             }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string responseMessage = string.IsNullOrEmpty(msg)
+                ? "This HTTP triggered function executed successfully. Coordinates saved in the database"
+                : $"This HTTP triggered function executed successfully. However, {msg}";
 
             return new OkObjectResult(responseMessage);
         }
